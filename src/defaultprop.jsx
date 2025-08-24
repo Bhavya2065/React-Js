83c2ac5c5a62f2dd8da92c6b29fef50b116b7b86
Bhavya <sanjivdani1@gmail.com>
2025-08-24 10:29:42 +0530

    How to define default prop

--- src/defaultprop.jsx
+++ src/defaultprop.jsx
@@ -8,7 +8,7 @@ export function Default_Prop({name="New User"}){ // New User is a Default Prop.
 
 // Wrapper jsx -> give style
 
-export function Wrapper({children, color="red"}){
+export function Wrapper({children, color="red"}){ // It takes bydefault red color if color is not set
     return (
         <>
             <div style={{color: color, border: "10px solid green", width:"600px", padding: "5px", margin:"10px"}}>
