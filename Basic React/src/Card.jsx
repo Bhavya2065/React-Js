3416943ee1c1a2965e7fd939045457a260eccbd4
Bhavya2065 <sanjivdani1@gmail.com>
2025-12-01 22:12:53 +0530

    inline and external CSS

--- Basic React/src/Card.jsx
+++ Basic React/src/Card.jsx
@@ -3,7 +3,7 @@ import styles from './card.module.css'
 function Card() {
   return (
     <div className={styles.box} style={{background: "red"}}>
-        <h2 className={styles.title} style={{fontSize: "40px", fontWeight: "200"}}>Hello CSS Modules</h2>
+        <h2 className={styles["box-title"]} style={{fontSize: "40px", fontWeight: "200"}}>Hello CSS Modules</h2>
     </div>
   )
 }
@@ -11,3 +11,4 @@ function Card() {
 export default Card
 
 // Note: Inline CSS apply first and the external CSS applies
+// Note: when the class name have a '-' like box-title then We write that style like this styles["box-title"]
