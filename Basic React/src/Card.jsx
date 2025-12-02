18c1b3d1346a418221d96cd3dd2c64bdba4a9127
Bhavya2065 <sanjivdani1@gmail.com>
2025-12-02 09:14:31 +0530

    inline css and External css

--- Basic React/src/Card.jsx
+++ Basic React/src/Card.jsx
@@ -11,4 +11,5 @@ function Card() {
 export default Card
 
 // Note: Inline CSS apply first and the external CSS applies
+// Note: whenever we define inline style then then Syntax: {{key: "value"}}
 // Note: when the class name have a '-' like box-title then We write that style like this styles["box-title"]
