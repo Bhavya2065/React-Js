a3d85f6c5843c5f24ee1ebc6a8521f5c3c3fce9f
Bhavya2065 <sanjivdani1@gmail.com>
2025-12-04 18:24:29 +0530

    All Methods of passing props

--- Basic React/src/propes.jsx
+++ Basic React/src/propes.jsx
@@ -1,16 +1,15 @@
-// Method 1: Static Data sending
-
-export function Static_propes({data}){
-    return(
+export function Static_propes({ data }) {
+    return (
         <>
             <h1>{data}</h1>
         </>
     )
 }
 
-export default function Dynamic_propes({name, age}){
+export default function Dynamic_propes({ name, age }) {
     return (
         <>
+            <hr />
             <div>
                 <h1>{name}</h1>
                 <h1>{age}</h1>
@@ -19,9 +18,9 @@ export default function Dynamic_propes({name, age}){
     )
 }
 
-export function Object_propes({user}){
+export function Object_propes({ user }) {
     console.log(user);
-    return(
+    return (
         <>
             <hr />
             <h1>{user.name}</h1>
@@ -30,11 +29,31 @@ export function Object_propes({user}){
     )
 }
 
-export function Name({name}) {
+export function Name({ name }) {
+    return (
+        <>
+            <hr />  
+            {name && <h1>Student Name: {name}</h1>}
+            {/* This condition use: if the student name is provided in the state then only It prints this statement */}
+        </>
+    )
+}
+
+export function WithoutDestructuring(props) {
+    return (
+        <>
+            <hr />
+            <h1>{props.name}</h1>
+            <h1>{props.age}</h1>
+            {/* Here we pass the data without using destructuring of js like {data} as above example */}
+        </>
+    )
+}
+
+export function Default_Prop({name="New User"}){ // New User is a Default Prop.
     return(
         <>
-            {name && <h1>Student Name: {name}</h1>} 
-            {/* This condition use: if the student name is provided in the state then only It prints this statement */}
+            <h1>Hi, {name}</h1>
         </>
     )
 }
