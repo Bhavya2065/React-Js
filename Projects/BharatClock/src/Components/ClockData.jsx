6e3db2bc0b0016cacf2ff93dc54b3a537468fc3f
Bhavya2065 <sanjivdani1@gmail.com>
2025-12-23 21:43:35 +0530

    Remove specified file(s)

--- Projects/BharatClock/src/Components/ClockData.jsx
+++
@@ -1,5 +0,0 @@
-export default function ClockData() {
-    return <div>
-        This is the clock that shows the time In Bharat at all times.
-    </div>
-}
--- Projects/BharatClock/src/Components/CurrentTime.jsx
+++
@@ -1,6 +0,0 @@
-export default function CurrentTime() {
-    let time = new Date()
-    return <div>
-        This is the current time: {time.toLocaleString()}
-    </div>
-}
