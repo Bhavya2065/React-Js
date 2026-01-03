653b2532321d46c49f55d0d8a67ba47e6c617c62
Bhavya2065 <sanjivdani1@gmail.com>
2026-01-03 21:16:36 +0530

    How to use Loader in react from react router

--- React-Concepts-Learning/React-Router-Loader/src/main.jsx
+++ React-Concepts-Learning/React-Router-Loader/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import CreatePosts, { postAction } from './Components/CreatePosts.jsx'
 import PostList, { postLoader } from './Components/PostList.jsx'
@@ -10,16 +10,16 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      {path: "/", element: <PostList />, loader: postLoader},
-      {path: "/createPost", element: <CreatePosts />, action: postAction}
+      { path: "/", element: <PostList />, loader: postLoader },
+      { path: "/createPost", element: <CreatePosts />, action: postAction }
     ]
   }
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
 
 // Here in this structure, the App components are fixed means If we navigate to the other page then these App component stays intact. It doesn't remove.
