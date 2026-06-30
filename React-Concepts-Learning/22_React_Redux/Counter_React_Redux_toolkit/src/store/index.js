526dc495e302f6cd5d702275e193905fb1dbdbc0
Bhavya2065 <sanjivdani1@gmail.com>
2026-06-30 17:13:39 +0530

    update

--- React-Concepts-Learning/22_React_Redux/Counter_React_Redux_toolkit/src/store/index.js
+++ React-Concepts-Learning/22_React_Redux/Counter_React_Redux_toolkit/src/store/index.js
@@ -2,11 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counter';
 import privacySlice from './privacy';
 
-const INITIAL_VALUE = {
-    counter: 0,
-    privacy: false
-} // If it has a many key-value pairs then How can we manage?
-
 const counterStore = configureStore({
     reducer: {
         counter: counterSlice.reducer,
